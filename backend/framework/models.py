b977936e31d697f02cefa5a5e6b6663f296b5a96
DanRodJim <dgrj.tec@gmail.com>
2025-08-25 18:25:54 -0600

    List of monsters and get monster endpoints by DanRodJim

--- backend/framework/models.py
+++ backend/framework/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.postgresql import JSONB
 
 Base = declarative_base()
 
@@ -7,3 +8,11 @@ class MotivationalPhrase(Base):
     __tablename__ = 'motivational_phrases'
     id = Column(Integer, primary_key=True)
     phrase = Column(String)
+
+class MonsterDanrodjim(Base):
+    __tablename__ = 'monsters_danrodjim'
+    id = Column(Integer, primary_key=True)
+    index = Column(String, unique=True, nullable=False)
+    name = Column(String, unique=True, nullable=False)
+    url = Column(String, unique=True, nullable=False)
+    data = Column(JSONB)
