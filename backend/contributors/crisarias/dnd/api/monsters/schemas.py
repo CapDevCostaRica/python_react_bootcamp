487bb9952fe5e27eaf0580057bc47c089a855d43
Cristian Arias <cristian.arias.chaves@hotmail.com>
2025-08-25 21:42:15 -0600

    Update schema validation

--- backend/contributors/crisarias/dnd/api/monsters/schemas.py
+++ backend/contributors/crisarias/dnd/api/monsters/schemas.py
@@ -1,5 +1,5 @@
 from services.telemetry import setupLogger
-from marshmallow import Schema, fields, INCLUDE
+from marshmallow import Schema, fields,validates, INCLUDE, ValidationError
 
 logger = setupLogger()
 
@@ -12,7 +12,21 @@ class MonstersCrisariasSchema(Schema):
         unknown = INCLUDE  # Include unknown fields (optional properties not defined in the schema)
         
 class ListMonstersSchema(Schema):
-    resource = fields.Str(required=True)
-        
+    resource = fields.Str(
+        required=True
+    )
+    
+    @validates('resource')
+    def validate_resource(self, value, data_key: str):
+        if not value or not value.strip():
+            raise ValidationError(f'{data_key} is required.')
+        if data_key == 'resource' and value != 'monsters':
+            raise ValidationError('Only monsters resource is valid.')
+
 class GetMonsterSchema(Schema):
     monster_index = fields.Str(required=True)
+
+    @validates('monster_index')
+    def validate_monster_index(self, value, data_key: str):
+        if not value or not value.strip():
+            raise ValidationError(f'{data_key} is required.')
