35ef8b7bcf2a67c3c0fec19ac185df3a7e60bf9a
Diego Murillo <diego.murillop@gmail.com>
2025-08-27 10:30:29 -0600

    Fixing Conflict in Models

--- backend/framework/models.py
+++ backend/framework/models.py
@@ -5,20 +5,30 @@ Base = declarative_base()
 
 
 class MotivationalPhrase(Base):
-    __tablename__ = 'dmuri_motivational_phrases'
+    __tablename__ = 'motivational_phrases'
     id = Column(Integer, primary_key=True)
     phrase = Column(String)
 
+class AllMonsterscastroulloaaaron(Base):
+    __tablename__ = 'castroulloaaaron_allmonsters'
+    id = Column(Integer, primary_key=True)
+    json_data = Column(JSON, nullable=False)
+
+
+class Monsterscastroulloaaaron(Base):
+    __tablename__ = 'castroulloaaaron_monsters'
+    id = Column(String, primary_key=True)
+    json_data = Column(JSON, nullable=False)
 
 class DMuriMonstersList(Base):
-    __tablename__ = 'dmrui_monsters_list'
+    __tablename__ = 'dmuri_monsters_list'
     id = Column(Integer, primary_key=True)
     data = Column(JSON, nullable=False)
     def to_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 class DMuriMonster(Base):
-    __tablename__ = 'monsters'
+    __tablename__ = 'dmuri_monsters'
     index = Column(String(100), primary_key=True)
     data = Column(JSON, nullable=False)
     def to_dict(self):
