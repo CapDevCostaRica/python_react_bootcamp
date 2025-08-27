f4ed956247c494c5869d174b716030d823d69423
DanRodJim <dgrj.tec@gmail.com>
2025-08-27 14:57:14 -0600

    Fix

--- backend/framework/scripts/populate_database.py
+++ backend/framework/scripts/populate_database.py
@@ -5,7 +5,6 @@ def run_alembic_upgrade(path):
     print(f"Running alembic upgrade head in {path}")
     subprocess.run(["alembic", "upgrade", "head"], cwd=path, check=True)
 
-
 if __name__ == "__main__":
     framework_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     run_alembic_upgrade(framework_dir)
