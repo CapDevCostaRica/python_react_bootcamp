c99cb41c8192fb48530286ec6d8b56dccf7644be
Cristian Arias <cristian.arias.chaves@hotmail.com>
2025-08-26 21:12:32 -0600

    Update schemas with schema and rules defined on dnd docs.

--- backend/contributors/crisarias/dnd/api/monsters/schemas.py
+++ backend/contributors/crisarias/dnd/api/monsters/schemas.py
@@ -6,48 +6,64 @@ logger = setupLogger()
 class MonstersCrisariasSimplifiedSchema(Schema):
     index = fields.Str(required=True)
     name = fields.Str(required=True)
-    url = fields.Str(required=True)
+    url = fields.Str(required=True)    
 
 class MonstersCrisariasSchema(Schema):
     index = fields.Str(required=True)
     name = fields.Str(required=True)
     url = fields.Str(required=True)
-    actions = fields.List(fields.Dict(), allow_none=True)
+    updated_at = fields.Str()
+    desc = fields.Str()
+    charisma = fields.Int()
+    constitution = fields.Int()
+    dexterity = fields.Int()
+    intelligence = fields.Int()
+    strength = fields.Int()
+    wisdom = fields.Int()
+    image = fields.Str()
+    size = fields.Str()
+    type = fields.Str()
+    subtype = fields.Str()
     alignment = fields.Str()
     armor_class = fields.List(fields.Dict(), allow_none=True)
-    challenge_rating = fields.Int()
-    charisma = fields.Int()
-    condition_immunities = fields.List(fields.Dict(), allow_none=True)
-    constitution = fields.Int()
-    damage_immunities = fields.List(fields.Dict(), allow_none=True)
-    damage_resistances = fields.List(fields.Dict(), allow_none=True)
-    damage_vulnerabilities = fields.List(fields.Dict(), allow_none=True)
-    dexterity = fields.Int()
-    forms = fields.List(fields.Dict(), allow_none=True)
-    hit_dice = fields.Str()
     hit_points = fields.Int()
+    hit_dice = fields.Str()    
     hit_points_roll = fields.Str()
-    image = fields.Str()
-    intelligence = fields.Int()
-    languages =  fields.Str()
+    actions = fields.List(fields.Dict(), allow_none=True)
     legendary_actions = fields.List(fields.Dict(), allow_none=True)
-    proficiencies = fields.List(fields.Dict(), allow_none=True)
+    challenge_rating = fields.Int()
     proficiency_bonus = fields.Int()
-    reactions = fields.List(fields.Dict(), allow_none=True)
+    condition_immunities = fields.List(fields.Dict(), allow_none=True)
+    damage_immunities = fields.List(fields.String(), allow_none=True)
+    damage_resistances = fields.List(fields.String(), allow_none=True)
+    damage_vulnerabilities = fields.List(fields.String(), allow_none=True)
+    forms = fields.List(fields.Dict(), allow_none=True)
+    languages =  fields.Str()
+    proficiencies = fields.List(fields.Dict(), allow_none=True)
+    reactions = fields.List(fields.Dict(), allow_none=True)    
     senses = fields.Dict(allow_none=True)
-    size = fields.Str()
     special_abilities = fields.List(fields.Dict(), allow_none=True)
-    speed = fields.Dict(allow_none=True)
-    strength = fields.Int()
-    type = fields.Str()
-    updated_at = fields.Str()
+    dc = fields.Dict()
+    spellcasting = fields.Dict()
+    usage  = fields.Dict()
+    speed = fields.Dict()
     xp = fields.Int()
-    wisdom = fields.Int()
-    
-    @validates('challenge_rating', 'charisma', 'constitution', 'dexterity', 'hit_points', 'intelligence', 'proficiency_bonus', 'strength', 'wisdom', 'xp')
-    def validate_attributes(self, value, data_key: str):
-        if value is not None and value <= 0:
-            raise ValidationError(f'{data_key} must be greater than zero.')
+
+    @validates('size', 'challenge_rating', 'proficiency_bonus')
+    def validate_resource(self, value, data_key: str):
+        if not value:
+            return True
+        if data_key == 'size' and value:  # Only validate if value exists
+            valid_sizes = ['Tiny', 'Small', 'Medium', 'Large', 'Huge', 'Gargantuan']
+            if value not in valid_sizes:
+                raise ValidationError(f'Size must be one of: {", ".join(valid_sizes)}')
+        if data_key == 'challenge_rating' and value:
+            if value > 21:
+                raise ValidationError('Challenge rating must be more than 21.')
+        if data_key == 'proficiency_bonus' and value:
+            if value < 2 or value > 9:
+                raise ValidationError('Proficiency bonus must be between 2 and 9.')
+  
 
     class Meta:
         unknown = EXCLUDE  # Include unknown fields (optional properties not defined in the schema)
