1247f7cd300c3e03feeeedd241abf65f259f246a
Daniel <daniel.madriz.h@gmail.com>
2025-08-27 14:28:53 -0600

    package update

--- backend/contributors/danielmadriz/dnd/src/application/__init__.py
+++ backend/contributors/danielmadriz/dnd/src/application/__init__.py
@@ -3,6 +3,9 @@ Application Layer - Business logic and validation.
 """
 from .service import MonsterService
 
+from .validators import MonsterValidator
+
 __all__ = [
-    'MonsterService'
-] 
+    'MonsterService',
+    'MonsterValidator'
+]
