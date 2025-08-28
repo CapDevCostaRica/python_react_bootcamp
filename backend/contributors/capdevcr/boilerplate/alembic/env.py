7fe15d57251578fec52e7d8d323dc9333407aa03
MandrakeCR <enrique.mendez@capdevcr.com>
2025-08-27 18:01:44 -0600

    Fix imports for models in boilerplate

--- backend/contributors/capdevcr/boilerplate/alembic/env.py
+++ backend/contributors/capdevcr/boilerplate/alembic/env.py
@@ -19,7 +19,7 @@ if config.config_file_name is not None:
 # add your model's MetaData object here
 # for 'autogenerate' support
 try:
-    from models import Base
+    from app.models import Base
 except ModuleNotFoundError:
     from app.framework.models import Base
 target_metadata = Base.metadata
