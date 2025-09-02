84a4ef720dc78f3f4a4d760edbaa406327751406
majocr <mariejo03cr@gmail.com>
2025-09-02 15:07:39 -0600

    Add extra test and apply feedback.

--- backend/contributors/majocr/file_processor/main.py
+++ backend/contributors/majocr/file_processor/main.py
@@ -2,7 +2,7 @@ import os
 import sys
 from flask import Flask, jsonify, request
 
-from app.service import query_people_by_filters
+from app.service import avg_height_nationality_general, get_avg_weight_above_70_by_hair, get_avg_weight_by_nationality_and_hair, get_most_common_food, get_people_like_shushi_and_ramen, get_top_oldest_natuinality, query_people_by_filters, top_hobbies
 from app.schema import PeopleResponseSchema
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../framework')))
 from app.config.seed_config import get_seed_config
@@ -98,7 +98,62 @@ def find_people():
     validated_data = schema.load(response_data)
     return jsonify(validated_data)
 
+#Extra 1
+#People who like both sushi and ramen
+@app.route('/people/sushi_ramen', methods=['GET'])
+def get_sushi_and_ramen():
+    session = get_session()
+    count = get_people_like_shushi_and_ramen(session)
+    return jsonify({"success": True, "data": count})
+
+#Extra 2
+#People with average weight above 70 grouped by hair color
+@app.route('/people/avg_weight_above_70_hair', methods=['GET'])
+def get_avg_weight_above_70_hair():
+    session = get_session()
+    data = get_avg_weight_above_70_by_hair(session)
+    return jsonify({"success": True, "data": data})
+
+#Extra 3
+#Most common food overall
+@app.route('/people/most_common_food_overall', methods=['GET'])
+def get_most_common_food_overall():
+    session = get_session()
+    data = get_most_common_food(session)
+    return jsonify(data)
+
+#Extra 4
+#Average weight grouped by nationality and hair color
+@app.route('/people/avg_weight_nationality_hair', methods=['GET'])
+def get_avg_weight_nationality_hair():
+    session = get_session()
+    data = get_avg_weight_by_nationality_and_hair(session)
+    return jsonify(data)
+
+#Extra 5
+#The top 2 oldest people per nationality
+@app.route('/people/top_oldest_nationality', methods=['GET'])
+def  top_oldest_nationality():
+    session = get_session()
+    data = get_top_oldest_natuinality(session)
+    return jsonify(data)
+
+#Extra 6
+#People ranked by how many hobbies they have (Top 3)
+@app.route('/people/top_hobbies', methods=['GET'])
+def get_top_hobbies():
+    session = get_session()
+    data = top_hobbies(session)
+    return jsonify(data)
+
+#Extra 7
+#Average height by nationality and average in general
+@app.route("/people/avg_height_nationality_general")
+def get_avg_height_nationality_general():
+    session = get_session()
+    data = avg_height_nationality_general(session)
+    return jsonify(data)
+
 if __name__ == '__main__':
     session = get_session()
-    is_data_seeded(session)
     app.run(host='0.0.0.0', port=4000)
